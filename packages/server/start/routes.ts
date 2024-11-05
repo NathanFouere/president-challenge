889a5a5131368d6520421c92fe688157ea44e4e0
Nathan Fouéré <nathanfouere@tutanota.com>
2024-11-05 21:32:13 +0100

    add more conf

--- packages/server/start/routes.ts
+++ packages/server/start/routes.ts
@@ -1,30 +1,21 @@
 /*
 |--------------------------------------------------------------------------
-| Routes
+| Routes file
 |--------------------------------------------------------------------------
 |
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes.ts` as follows
-|
-| import './routes/cart'
-| import './routes/customer'
+| The routes file is used for defining the HTTP routes.
 |
 */
 
-import Route from '@ioc:Adonis/Core/Route';
-import { Test } from "@shared/types/test";
+import router from '@adonisjs/core/services/router';
+
+import { Test } from '@shared/types/test.js';
 
 const test: Test = {
-  test: 'Coucou'
+  test: 'test',
 };
-
-Route.get('/', async () => {
+router.get('/test', async () => {
   return test;
 });
+
+router.on('/').render('pages/home');
