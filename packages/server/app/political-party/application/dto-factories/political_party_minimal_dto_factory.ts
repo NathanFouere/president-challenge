fa7ef3136f6d6bb371abf0448658c3f62d71cd1d
Nathan Fouéré <nathanfouere@tutanota.com>
2024-12-15 13:47:13 +0100

    add order to political parties

--- packages/server/app/political-party/application/dto-factories/political_party_minimal_dto_factory.ts
+++ packages/server/app/political-party/application/dto-factories/political_party_minimal_dto_factory.ts
@@ -2,6 +2,15 @@ import type { PoliticalPartyMinimalDto } from '@shared/types/political-party/pol
 import type PoliticalParty from '#political-party/domain/models/political_party';
 
 export class PoliticalPartyMinimalDTOFactory {
+  readonly AffiliationOrder = {
+    'Far Left': 0,
+    'Left': 1,
+    'Center Left': 2,
+    'Center Right': 3,
+    'Right': 4,
+    'Far Right': 5,
+  };
+
   public createPoliticalPartyMinimalDTO(politicalParty: PoliticalParty): PoliticalPartyMinimalDto {
     return {
       id: politicalParty.id,
@@ -12,6 +21,7 @@ export class PoliticalPartyMinimalDTOFactory {
   }
 
   public createPoliticalPartyMinimalDTOList(politicalParties: PoliticalParty[]): PoliticalPartyMinimalDto[] {
+    politicalParties.sort((a, b) => this.AffiliationOrder[a.affiliation] - this.AffiliationOrder[b.affiliation]);
     return politicalParties.map(this.createPoliticalPartyMinimalDTO);
   }
 }
