6233b34487c490239fab5a7409b0a65638bba89e
Nathan Fouéré <nathanfouere@tutanota.com>
2025-01-04 22:02:39 +0100

    move range levels

--- packages/server/app/social-class/application/dto-factories/social-class-dto-factory.ts
+++ packages/server/app/social-class/application/dto-factories/social-class-dto-factory.ts
@@ -5,12 +5,7 @@ import type SocialClass from '#social-class/domain/models/social_class';
 import { LicensedFileDTOFactory } from '#licensed-file/application/factory/licensed_file_dto_factory';
 // eslint-disable-next-line @typescript-eslint/consistent-type-imports
 import ChartDataFactory from '#common/utils/chart_data_factory';
-import { socialClassHappinessRangeLevels } from '#social-class/domain/range-levels/social_class_happiness_range_levels';
-import {
-  socialClassEconomicalSituationRangeLevels,
-} from '#social-class/domain/range-levels/social_class_economical_situation_range_levels';
-// eslint-disable-next-line @typescript-eslint/consistent-type-imports
-import RangeLevelMatch from '#common/utils/range_level_match';
+import type RangeLevelMatch from '#common/utils/range_level_match';
 
 @inject()
 export class SocialClassDtoFactory {
@@ -21,14 +16,30 @@ export class SocialClassDtoFactory {
   ) {
   }
 
+  readonly socialClassEconomicalSituationRangeLevels = [
+    { min: 0, max: 0, value: 'Very-Low' },
+    { min: 1, max: 1, value: 'Low' },
+    { min: 2, max: 2, value: 'Medium' },
+    { min: 3, max: 3, value: 'High' },
+    { min: 4, max: 4, value: 'Very-High' },
+  ];
+
+  readonly socialClassHappinessRangeLevels = [
+    { min: 0, max: 0, value: 'Very-Low' },
+    { min: 1, max: 1, value: 'Low' },
+    { min: 2, max: 2, value: 'Medium' },
+    { min: 3, max: 3, value: 'High' },
+    { min: 4, max: 4, value: 'Very-High' },
+  ];
+
   public createFromSocialClass(socialClass: SocialClass): SocialClassDto {
     return {
       id: socialClass.id,
       name: socialClass.name,
       description: socialClass.description,
       color: socialClass.color,
-      economicalSituation: this.rangeLevelMatch.createFromAmountPerTurn(socialClass.economicalSituation, socialClassEconomicalSituationRangeLevels),
-      happinessLevel: this.rangeLevelMatch.createFromAmountPerTurn(socialClass.happinessLevel, socialClassHappinessRangeLevels),
+      economicalSituation: this.rangeLevelMatch.createFromAmountPerTurn(socialClass.economicalSituation, this.socialClassEconomicalSituationRangeLevels),
+      happinessLevel: this.rangeLevelMatch.createFromAmountPerTurn(socialClass.happinessLevel, this.socialClassHappinessRangeLevels),
       socialClassType: socialClass.subType,
       licensedFiles: this.licensedFileDTOFactory.createFromLicensedFiles(socialClass.licensedFiles),
       happinessPerMonthChartData: this.chartDataFactory.createFromAmountPerTurn(
@@ -36,14 +47,14 @@ export class SocialClassDtoFactory {
         'Happiness Level',
         0,
         4,
-        socialClassHappinessRangeLevels,
+        this.socialClassHappinessRangeLevels,
       ),
       economicalSituationPerMonthChartData: this.chartDataFactory.createFromAmountPerTurn(
         socialClass.economicalSituationPerTurn,
         'Economical Situation',
         0,
         4,
-        socialClassEconomicalSituationRangeLevels),
+        this.socialClassEconomicalSituationRangeLevels),
     };
   }
 }
