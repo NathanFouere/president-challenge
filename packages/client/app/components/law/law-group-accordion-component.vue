d014a730f9642999e5f998223f7385053cd1b73f
Nathan Fouéré <nathanfouere@tutanota.com>
2025-01-11 22:17:51 +0100

    separe law group accordion and item component

--- packages/client/app/components/law/law-group-accordion-component.vue
+++ packages/client/app/components/law/law-group-accordion-component.vue
@@ -21,25 +21,6 @@ const items: LawGroupAccordionItem[] = props.lawCategory.lawGroups.map((lawGroup
     propertyLaws: lawGroup.propertyLaws,
   };
 });
-
-const columns = [
-  {
-    key: 'name',
-    label: 'Name',
-  },
-  {
-    key: 'description',
-    label: 'Description',
-  },
-  {
-    key: 'voted',
-    label: 'Voted',
-  },
-  {
-    key: 'actions',
-    label: 'Actions',
-  },
-];
 </script>
 
 <template>
@@ -48,31 +29,7 @@ const columns = [
     :items="items"
   >
     <template #item="{ item }">
-      <div>
-        <UTable
-          :rows="item.propertyLaws"
-          :columns="columns"
-          :empty-state="{ icon: '', label: 'No laws' }"
-          class="text-left"
-        >
-          <template #voted-data="{ row }">
-            <span>
-              <UIcon
-                :name="row.voted ? 'i-heroicons-check' : 'i-heroicons-x-mark'"
-                class="text-green"
-              />
-              {{ row.voted ? 'Voted' : 'Not voted' }}
-            </span>
-          </template>
-          <template #actions-data="{ row }">
-            <law-modal
-              :law-id="row.id"
-              :already-voted="row.voted"
-              :type="row.type"
-            />
-          </template>
-        </UTable>
-      </div>
+      <law-accordion-item-component :rows="item.propertyLaws" />
     </template>
   </UAccordion>
 </template>
