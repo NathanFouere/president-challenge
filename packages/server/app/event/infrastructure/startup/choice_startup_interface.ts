1f7b0a3282df6f5dea07aee70d4bb9131f62d094
Nathan Fouéré <nathanfouere@tutanota.com>
2025-02-15 19:17:28 +0100

    add admin and remove lots of config

--- packages/server/app/event/infrastructure/startup/choice_startup_interface.ts
+++
@@ -1,9 +0,0 @@
-import type { ChoiceStatus } from '@shared/dist/event/choice-status.js';
-import type { EventStartupInterface } from '#event/infrastructure/startup/event_startup_interface';
-
-export interface ChoiceStartupInterface {
-  text: string;
-  status: ChoiceStatus;
-  event: EventStartupInterface;
-  triggerEventIdentifier: string;
-}
--- packages/server/app/law/infrastructure/startup/startup-interface/law_category_startup_interface.ts
+++
@@ -1,9 +0,0 @@
-import type {
-  LawGroupStartupInterface,
-} from '#law/infrastructure/startup/startup-interface/law_group_startup_interface';
-
-export default interface LawCategoryStartupInterface {
-  name: string;
-  description: string;
-  lawGroups: LawGroupStartupInterface[];
-}
