3616d8200f38c569c8fbe94ec0e9a520ba11c025
Nathan Fouéré <nathanfouere@tutanota.com>
2025-02-15 18:59:40 +0100

    small fixes

--- packages/server/app/event/domain/models/event.ts
+++ packages/server/app/event/domain/models/event.ts
@@ -1,11 +1,12 @@
 import { BaseModel, belongsTo, column, hasMany } from '@adonisjs/lucid/orm';
 import type { BelongsTo, HasMany } from '@adonisjs/lucid/types/relations';
+import type { DateTime } from 'luxon';
 import Game from '#game/domain/models/game';
 import EventDefinition from '#event/domain/models/event_definition';
 import Choice from '#event/domain/models/choice';
 
 export default class Event extends BaseModel {
-  @column({ isPrimary: true, serializeAs: null })
+  @column({ isPrimary: true })
   declare id: number;
 
   @column()
@@ -36,4 +37,10 @@ export default class Event extends BaseModel {
 
   @hasMany(() => Choice)
   declare choices: HasMany<typeof Choice>;
+
+  @column.dateTime({ autoCreate: true, serializeAs: null })
+  declare createdAt: DateTime;
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true, serializeAs: null })
+  declare updatedAt: DateTime | null;
 }
