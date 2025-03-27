bf4e430e309c1a86028bfbd6804358de0858c1e9
Nathan Fouéré <nathanfouere@tutanota.com>
2025-03-27 20:37:50 +0100

    add user dto

--- packages/server/app/user/infrastructure/controllers/login_controller.ts
+++ packages/server/app/user/infrastructure/controllers/login_controller.ts
@@ -1,7 +1,16 @@
 import type { HttpContext } from '@adonisjs/core/http';
+import { inject } from '@adonisjs/core';
 import User from '#user/domain/models/user';
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+import UserDtoFactory from '#user/application/dto-factory/user_dto_factory';
 
+@inject()
 export default class LoginController {
+  constructor(
+    private readonly userDtoFactory: UserDtoFactory,
+  ) {
+  }
+
   async login({ request, auth, response }: HttpContext) {
     const { email, password } = request.only(['email', 'password']);
 
@@ -10,7 +19,8 @@ export default class LoginController {
 
       await auth.use('web').login(user);
 
-      return user;
+      const userDto = this.userDtoFactory.createFromUser(user);
+      return userDto;
     }
     catch {
       return response.unauthorized('Invalid credentials');
